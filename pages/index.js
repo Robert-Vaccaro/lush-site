aafb52ae9ff47853e2882047ab132ed2963d94bc
robert-vaccaro <robertvaccarotech@gmail.com>
2023-06-01 09:09:40 -0700

    hopefully fixed routing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
-
+import Image from 'next/image'
+import { useRouter } from "next/router";
+import {Card, CardActions, CardContent, Button, Typography} from '@mui/material'
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
-      No
+      <Image
+      style={{width:"100%", minWidth:"800px", height:"100vh", minHeight:"500px", objectFit:"fill"}}
+       src={require("../images/banner.png")}
+       alt='lush-image' 
+       priority={false}
+       onClick={() => router.push(`login`)}/>
+
+     <Button onClick={() => router.push(`login`)} variant="contained" style={{position:"absolute", left:"50%", top:"80%"}} >Login</Button>
     </div>
   )
 }
